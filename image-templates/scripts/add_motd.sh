af0438dab47cdec6f7376cc9bc63226ced270c3c
vgnshiyer <vgnshiyer@gmail.com>
2024-04-03 17:07:22 -0700

    chore: syntax error in sudoers

--- image-templates/scripts/add_motd.sh
+++ image-templates/scripts/add_motd.sh
@@ -7,4 +7,4 @@ echo -e "\e[93mWelcome to \e[36m
  | |__| (_) | | | | || (_| | | | | |  __/ | |_____| |\\  |  __/ |_ 
   \\____\\___/|_| |_|\\__\\__,_|_|_| |_|\\___|_|       |_| \\_|\\___|\\__|
 
-\e[93mWe hope you have a great experience. If you need any assistance, please visit our help page at https://www.github.com/vgnshiyer/Container-Net." > /etc/motd
+\e[93mWe hope you have a great experience. \nIf you need any assistance, please visit our help page at https://www.github.com/vgnshiyer/Container-Net.\e[0m" > /etc/motd
--- image-templates/scripts/create_users.sh
+++ image-templates/scripts/create_users.sh
@@ -8,5 +8,5 @@ for i in "${GUESTS[@]}"; do
     IFS=':' read -ra USER <<< "$i"
     useradd -m -d /home/${USER[0]} -s /bin/bash ${USER[0]}
     echo "${USER[0]}:${USER[1]}" | chpasswd
-    echo "${USER[0]} ALL:(ALL) ALL" >> /etc/sudoers
+    echo "${USER[0]} ALL=(ALL:ALL) ALL" >> /etc/sudoers
 done
